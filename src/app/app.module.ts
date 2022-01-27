4355d1f05cf4eabc93a6da76081b9ad60d331926
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-01-27 00:13:34 -0300

    chore: initial project application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/courses-list.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/courses/courses-list.components.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector:'app-course-list',
+  templateUrl: './courses-list.components.html'
+})
+export class CourseListComponent {
+
+
+
+}
