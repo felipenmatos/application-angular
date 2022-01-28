c9ec1587e60caf61fa8fe2fd25d9a9c3bc85cccf
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-01-28 00:52:29 -0300

    chore: config bootstrap

--- src/app/courses/courses-list.components.ts
+++ src/app/courses/courses-list.components.ts
@@ -1,12 +1,36 @@
 import { Component } from "@angular/core";
+import { Course } from "./course";
 
 
 @Component({
   selector:'app-course-list',
   templateUrl: './courses-list.components.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
 
+  courses: Course[] = [];
 
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 5.4
+      },
+      {
+        id: 2,
+        name: 'Angular: Aplication',
+        imageUrl: '',
+        price: 45.99,
+        code: 'LKL 1994',
+        duration: 80,
+        rating: 4
+      }
+    ]
+  }
 
 }
